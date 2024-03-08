81cb93959d36baa8abae7811206276fe3a319783
Madhu <mrs.madhusmita.mishra@gmail.com>
2024-03-08 13:31:30 +0200

    Passing Event Handlers to Child Components done.

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,9 @@ const History = (props) => {
     </div>
   )
 }
-const Button = ({ handleClick, text }) => (
-  <button onClick={handleClick}>
-    {text}
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
   </button>
 )
 
@@ -70,15 +70,9 @@ const App = () => {
       <hr></hr>
 
       {value}
-      <button onClick={() => setToValue(1000)}>
-        thousand
-      </button>
-      <button onClick={() => setToValue(0)}>
-        reset
-      </button>
-      <button onClick={() => setToValue(value + 1)}>
-        increment
-      </button>
+      <Button handleClick={() => setToValue(1000)} text="thousand" />
+      <Button handleClick={() => setToValue(0)} text="reset" />
+      <Button handleClick={() => setToValue(value + 1)} text="increment" />
     </div>
   )
 }
